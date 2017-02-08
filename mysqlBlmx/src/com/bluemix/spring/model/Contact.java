94a6a801ada9b6423047b7ecc95e822d137ce941
Ziv Dai <zivd@il.ibm.com>
2017-02-08 21:02:16 +0200

    setter ang getter

--- mysqlBlmx/src/com/bluemix/spring/model/Contact.java
+++ mysqlBlmx/src/com/bluemix/spring/model/Contact.java
@@ -16,6 +16,61 @@ public class Contact {
         this.address = address;
         this.telephone = telephone;
     }
+
+	public void setId(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEmail(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAddress(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setTelephone(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int get() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getTelephone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
  
     // getters and setters
 }
